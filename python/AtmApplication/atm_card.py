f5d5130b99dd2948fc1a0c4b0de00695c494c7de
ansyfirmansyah <35186924+ansyfirmansyah@users.noreply.github.com>
2021-03-15 18:25:29 +0700

    Update atm_card.py

--- python/AtmApplication/atm_card.py
+++ python/AtmApplication/atm_card.py
@@ -1,8 +1,11 @@
 class ATMCard:
+    # Initialize PIN and Balance
     def __init__(self, defaultPin, defaultBalance):
         self.defaultPin = defaultPin
         self.defaultBalance = defaultBalance
+    # Validate PIN of ATM
     def cekPinAwal(self):
         return self.defaultPin
+    # Show current balance
     def cekSaldoAwal(self):
         return self.defaultBalance
